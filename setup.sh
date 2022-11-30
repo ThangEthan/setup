ef36f202e4d74e31d84cb84b485230a5826dcbe8
Vu Viet Thang <thangvu201098@gmail.com>
2022-11-30 12:27:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 sudo apt-get update;
 sudo apt-get install -y zsh git cargo nodejs pip curl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
