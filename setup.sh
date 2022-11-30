a26898a33c73f59bed44f3d47e9f391706a5622b
Vu Viet Thang <thangvu201098@gmail.com>
2022-11-30 12:28:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ sudo apt-get update;
 sudo apt-get install -y zsh git cargo nodejs pip curl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install k9s neovim
