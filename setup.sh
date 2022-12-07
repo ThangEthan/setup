2fb885b00cc1456d5aab019804f69081dee4844d
Vu Viet Thang <thangvu201098@gmail.com>
2022-12-07 12:32:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update;
-sudo apt-get install -y zsh git cargo nodejs pip curl unzip
+sudo apt-get install -y zsh git cargo nodejs pip curl unzip build-essentials
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install k9s neovim awsvault
