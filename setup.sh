92036f3e981578dec9ad3b8df59bfa0647538b12
Vu Viet Thang <thangvu201098@gmail.com>
2022-12-07 13:59:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt-get update;
 sudo apt-get install -y zsh git cargo nodejs pip curl unzip build-essentials
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install k9s neovim aws-vault
+brew install k9s neovim aws-vault lazygit
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
