2f12744ebb0ce49c466ca9e4403b9e6056722b9c
Vu Viet Thang <thangvu201098@gmail.com>
2022-12-08 10:27:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 sudo apt-get update;
-sudo apt-get install -y zsh git cargo pip curl unzip build-essential
+sudo apt-get install -y zsh git cargo pip curl unzip build-essential ca-certificates
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
